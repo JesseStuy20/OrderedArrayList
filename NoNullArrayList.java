5c25a74aad5473a43585232883e8aad1d4a2f149
JesseStuy20 <jxie20@stuy.edu>
2020-12-10 13:15:28 -0500

    Added override for secondary add method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -5,8 +5,14 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     if (element == null) {
       throw new IllegalArgumentException ("Value cannot be null") ;
       }
-    super.add(element) ;
-    return true ;
+    return super.add(element) ;
+    }
+
+    public void add(int index, T element) {
+      if (element == null) {
+        throw new IllegalArgumentException ("Value cannot be null") ;
+        }
+      super.add(index, element) ;
     }
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # OrderedArrayList
 
 - Discussed basic requirements for overriding add and set methods.
+- Discussed importing ArrayList and utilizing super.
