222ca8b3ca1da4d8679b1127adc4336984c380a5
JesseStuy20 <jxie20@stuy.edu>
2020-12-13 22:49:49 -0500

    Fixed code for add method and added more test cases

--- ListTester.java
+++ ListTester.java
@@ -11,6 +11,19 @@ public class ListTester {
     System.out.println(a.add("uno")) ;
     System.out.println(a.add("dos")) ;
     System.out.println(a.add("tres")) ;
+    OrderedArrayList<Integer> b = new OrderedArrayList<Integer>() ;
+    try {
+      b.add(null) ;
+    }
+    catch(IllegalArgumentException e) {
+      System.out.println("Element cannot be null") ;
+    }
+    System.out.println(b.add(1)) ;
+    System.out.println(b.add(45)) ;
+    System.out.println(b.add(-41)) ;
+    System.out.println(b.add(0)) ;
+    System.out.println(b.add(18)) ;
+    System.out.println(b.toString()) ;
   }
 
 }
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -4,7 +4,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public boolean add(T element) {
     super.add(element) ;
     for (int i=0; i<size(); i++) {
-      if (get(i).compareTo(get(size())) != 0) {
+      if (get(i).compareTo(get(size()-1)) > 0) {
         T temp = get(i) ;
         remove(i) ;
         super.add(temp) ;
